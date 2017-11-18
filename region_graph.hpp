3cf1e9e4d53158bf9bf3408c907e1fc9679d4016
Ran Lu <ranl@princeton.edu>
2017-11-18 14:31:33 -0500

    Only loop the non-overlapping part of the cube

--- region_graph.hpp
+++ region_graph.hpp
@@ -11,7 +11,7 @@ template< typename ID, typename F, typename L>
 inline region_graph_ptr<ID,F>
 get_region_graph( const affinity_graph_ptr<F>& aff_ptr,
                   const volume_ptr<ID> seg_ptr,
-                  std::size_t max_segid, const L& lowv)
+                  std::size_t max_segid, const L& lowv, const std::array<bool,6> & boundary_flags)
 {
     using affinity_t = F;
     using id_pair = std::pair<ID,ID>;
@@ -36,11 +36,11 @@ get_region_graph( const affinity_graph_ptr<F>& aff_ptr,
         h.reserve(10);
     }
 
-    for ( std::ptrdiff_t z = 0; z < zdim; ++z )
-        for ( std::ptrdiff_t y = 0; y < ydim; ++y )
-            for ( std::ptrdiff_t x = 0; x < xdim; ++x )
+    for ( std::ptrdiff_t z = 1; z < zdim - 1; ++z )
+        for ( std::ptrdiff_t y = 1; y < ydim - 1; ++y )
+            for ( std::ptrdiff_t x = 1; x < xdim - 1; ++x )
             {
-                if ( (x > 0) && seg[x][y][z] && seg[x-1][y][z] && seg[x][y][z] != seg[x-1][y][z])
+                if ( (x > boundary_flags[0]) && seg[x][y][z] && seg[x-1][y][z] && seg[x][y][z] != seg[x-1][y][z])
                 {
                     auto p = std::minmax(seg[x][y][z], seg[x-1][y][z]);
                     F& curr = edges[p.first][p.second];
@@ -49,7 +49,7 @@ get_region_graph( const affinity_graph_ptr<F>& aff_ptr,
                     }
                     curr = std::max(curr, aff[x][y][z][0]);
                 }
-                if ( (y > 0) && seg[x][y][z] && seg[x][y-1][z] && seg[x][y][z] != seg[x][y-1][z])
+                if ( (y > boundary_flags[1]) && seg[x][y][z] && seg[x][y-1][z] && seg[x][y][z] != seg[x][y-1][z])
                 {
                     auto p = std::minmax(seg[x][y][z], seg[x][y-1][z]);
                     F& curr = edges[p.first][p.second];
@@ -58,7 +58,7 @@ get_region_graph( const affinity_graph_ptr<F>& aff_ptr,
                     }
                     curr = std::max(curr, aff[x][y][z][1]);
                 }
-                if ( (z > 0) && seg[x][y][z] && seg[x][y][z-1] && seg[x][y][z] != seg[x][y][z-1])
+                if ( (z > boundary_flags[2]) && seg[x][y][z] && seg[x][y][z-1] && seg[x][y][z] != seg[x][y][z-1])
                 {
                     auto p = std::minmax(seg[x][y][z], seg[x][y][z-1]);
                     F& curr = edges[p.first][p.second];
