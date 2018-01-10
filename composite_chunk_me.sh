f224de6868cb99af18e3b279ef18fd8bbf5916ab
Ran Lu <ranl@princeton.edu>
2018-01-10 11:51:32 -0500

    Save the final region graph as well

--- composite_chunk_me.sh
+++ composite_chunk_me.sh
@@ -33,15 +33,18 @@ try mv meta.data meta_"$output".data
 try mv mst.data mst_"$output".data
 try mv remap.data remap_"$output".data
 try mv residual_rg.data residual_rg_"$output".data
+try mv final_rg.data final_rg_"$output".data
 
 try pbzip2 mst_"${output}".data
 try pbzip2 remap_"${output}".data
 try pbzip2 complete_edges_"${output}".data
+try pbzip2 final_rg_"${output}".data
 
 try $UPLOAD_CMD meta_"${output}".data $FILE_PATH/meta/meta_"${output}".data
 try $UPLOAD_CMD mst_"${output}".data.bz2 $FILE_PATH/mst/mst_"${output}".data.bz2
 try $UPLOAD_CMD remap_"${output}".data.bz2 $FILE_PATH/remap/remap_"${output}".data.bz2
 try $UPLOAD_CMD complete_edges_"${output}".data.bz2 $FILE_PATH/region_graph/complete_edges_"${output}".data.bz2
+try $UPLOAD_CMD final_rg_"${output}".data.bz2 $FILE_PATH/region_graph/final_rg_"${output}".data.bz2
 
 try tar --use-compress-prog=pbzip2 -cf "${output}".tar.bz2 *_"${output}".data
 try $UPLOAD_CMD "${output}".tar.bz2 $FILE_PATH/scratch/"${output}".tar.bz2
