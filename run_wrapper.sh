1c265bbf27d8f60b7c5f002215c47a9938c5de41
Ran Lu <ranl@princeton.edu>
2019-01-30 21:31:49 -0500

    Single thread is enough for one file

--- run_wrapper.sh
+++ run_wrapper.sh
@@ -6,7 +6,7 @@ OP=$2
 CHUNK=$3
 WORK_PATH="$(realpath "$1")"
 
-try_to_skip $DOWNLOAD_CMD $FILE_PATH/done/"$OP"/"$CHUNK".txt .
+try_to_skip $DOWNLOAD_ST_CMD $FILE_PATH/done/"$OP"/"$CHUNK".txt .
 
 set -euo pipefail
 
@@ -23,7 +23,7 @@ just_in_case rm -rf "$CHUNK"
 try mkdir "$CHUNK" && pushd "$CHUNK" && "$SCRIPT_PATH"/"$OP".sh "$WORK_PATH"/"$CHUNK".json && popd
 
 try touch "${CHUNK}".txt
-try $UPLOAD_CMD "${CHUNK}".txt $FILE_PATH/done/"$OP"/"${CHUNK}".txt
+try $UPLOAD_ST_CMD "${CHUNK}".txt $FILE_PATH/done/"$OP"/"${CHUNK}".txt
 
 try rm -rf "$CHUNK"
 try rm "$CHUNK".txt
