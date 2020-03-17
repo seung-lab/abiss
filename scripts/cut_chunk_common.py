b4360ce28e94424ba40e0de346377a1f9a3bac5b
Ran Lu <ranl@princeton.edu>
2020-03-16 20:49:19 -0400

    Add switch in json to enable/disable the closing operation

--- scripts/cut_chunk_common.py
+++ scripts/cut_chunk_common.py
@@ -1,4 +1,5 @@
 from cloudvolume import CloudVolume
+import chunk_utils as cu
 import numpy
 import os
 from scipy import ndimage
@@ -18,13 +19,16 @@ def fold_aff(a):
     return a
 
 def adjust_affinitymap(aff, bbox, boundary_flags, padding_before, padding_after):
-    #start_coord = [bbox[i]-(1-boundary_flags[i])*padding_before for i in range(3)]
-    #end_coord = [bbox[i+3]+(1-boundary_flags[i+3])*padding_after for i in range(3)]
-    #return fold_aff(cut_data(aff, start_coord, end_coord, boundary_flags))
-    erode_params = [[10,0.995],[10,0.995],[2,0.995]]
+    global_param = cu.read_inputs(os.environ['PARAM_JSON'])
+    if not global_param.get("CLOSING_AFF", False):
+        start_coord = [bbox[i]-(1-boundary_flags[i])*padding_before for i in range(3)]
+        end_coord = [bbox[i+3]+(1-boundary_flags[i+3])*padding_after for i in range(3)]
+        return fold_aff(cut_data(aff, start_coord, end_coord, boundary_flags))
+
+    erode_params = [[10,0.99],[10,0.99],[2,0.99]]
     start_coord = [bbox[i]-(1-boundary_flags[i])*(padding_before+erode_params[i][0]) for i in range(3)]
     end_coord = [bbox[i+3]+(1-boundary_flags[i+3])*(padding_after+erode_params[i][0]) for i in range(3)]
-    data = close_affinitymap(cut_data(aff, start_coord, end_coord, [0,0,0,0,0,0]), erode_params, 0.8)
+    data = close_affinitymap(cut_data(aff, start_coord, end_coord, [0,0,0,0,0,0]), erode_params, 0.5)
     #bb = tuple(slice(start_coord[i], end_coord[i]) for i in range(3))
     #data = aff[bb+(slice(0,3),)]
     start_coord = [(1-boundary_flags[i])*erode_params[i][0] for i in range(3)]
@@ -56,22 +60,35 @@ def erode_affinitymap(data, params, threshold):
 
 def close_affinitymap(data, params, threshold):
     #data[:,:,:,2] *= 0.99
-    #idx = data[:] > 0.9
-    #data[idx] = 0.9
+    idx = data[:] > 0.9
+    data[idx] *= 0.98
     for i in range(3):
-        mask = numpy.ones_like(data[:,:,:,i])
-        idx = (data[:,:,:,i] < threshold) & (data[:,:,:,i] > 0)
-        mask[idx] = 0
+        mask = numpy.logical_and(data[:,:,:,i] < threshold, data[:,:,:,i] > 0)
         step = -2
         if (i == 2):
             step = -2
         for r in (range(params[i][0]*2+1, 2, step)):
-            sz = [0,0,0]
-            sz[i] = r
-            print(sz)
-            lr_mask = ndimage.grey_opening(mask, size=sz)
-            lr_idx = lr_mask < threshold
-            data[lr_idx,:] *= params[i][1]
+            if i == 2:
+                s_z = numpy.zeros((r,r,r), dtype=int)
+                s_z[r//2,r//2,:] = 1
+                lr_mask = numpy.logical_and(ndimage.binary_closing(mask, structure=s_z), numpy.logical_not(mask))
+                data[lr_mask,i] *= params[i][1]
+            else:
+                s_xy = numpy.zeros((r,r,r), dtype=int)
+                s_xy[:,:,r//2] = ndimage.iterate_structure(ndimage.generate_binary_structure(2, 1),r//2).astype(int)
+                s_x = numpy.zeros((r,r,r), dtype=int)
+                s_x[r//2,:,r//2] = 1
+                s_y = numpy.zeros((r,r,r), dtype=int)
+                s_y[:,r//2,r//2] = 1
+                s_d = numpy.zeros((r,r,r), dtype=int)
+                s_d[:,:,r//2] = numpy.eye(r, dtype=int)
+                s_ad = numpy.zeros((r,r,r), dtype=int)
+                s_ad[:,:,r//2] = numpy.flipud(numpy.eye(r, dtype=int))
+                lr_mask_small = numpy.logical_or(numpy.logical_and(ndimage.binary_closing(mask, structure=s_x), ndimage.binary_closing(mask, structure=s_y)), numpy.logical_and(ndimage.binary_closing(mask, structure=s_d), ndimage.binary_closing(mask, structure=s_ad)))
+                lr_mask = ndimage.binary_closing(mask, structure=s_xy)
+                if r > 3:
+                    lr_mask = numpy.logical_and(lr_mask, numpy.logical_not(lr_mask_small))
+                data[lr_mask,i] *= params[i][1]
 
     return data
 
