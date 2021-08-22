c4adb2c4a6e12bcb6ac044789852d8a3fbeb7e73
Ran Lu <ranl@princeton.edu>
2021-08-21 23:06:11 -0400

    Fix cloudvolume error

--- scripts/upload_chunk.py
+++ scripts/upload_chunk.py
@@ -13,4 +13,4 @@ bbox = param["bbox"]
 sizes = tuple([bbox[i+3]-bbox[i] for i in range(3)])
 fp = np.memmap(fn, dtype=np.uint64, mode='r', shape=sizes, order='F')
 vol = CloudVolume(sys.argv[2], mip=global_param['AFF_RESOLUTION'], cdn_cache=False)
-vol[bbox[0]:bbox[3], bbox[1]:bbox[4], bbox[2]:bbox[5]] = fp[:]
+vol[bbox[0]:bbox[3], bbox[1]:bbox[4], bbox[2]:bbox[5]] = np.array(fp[:])
