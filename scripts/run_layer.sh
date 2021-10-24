28986f364030f3d4ace8abca9ec859ead2d93866
Ran Lu <ranl@princeton.edu>
2021-10-24 02:40:35 -0400

    Use PARALLEL_CMD

--- scripts/run_layer.sh
+++ scripts/run_layer.sh
@@ -8,9 +8,9 @@ try_to_skip $DOWNLOAD_ST_CMD $FILE_PATH/done/batch_"$2"_"$3"_"$1".txt .
 set -euo pipefail
 
 if [ "$1" = 0 ]; then
-    try cat "$1".txt | parallel --verbose $SCRIPT_PATH/run_wrapper.sh . "$2" {}
+    try cat "$1".txt | $PARALLEL_CMD $SCRIPT_PATH/run_wrapper.sh . "$2" {}
 elif [ "$1" = 1 ]; then
-    try cat "$1".txt | parallel --delay 1 --verbose $SCRIPT_PATH/run_wrapper.sh . "$2" {}
+    try cat "$1".txt | $PARALLEL_CMD $SCRIPT_PATH/run_wrapper.sh . "$2" {}
 elif [ "$1" = 2 ]; then
     try cat "$1".txt | parallel -j 8 --verbose $SCRIPT_PATH/run_wrapper.sh . "$2" {}
 else
