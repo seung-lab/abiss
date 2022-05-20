00bfcb3ab46ae6549c19924771eda43e8d2f3f3c
William Silversmith <william.silversmith@gmail.com>
2022-05-20 16:37:57 -0400

    perf: try a vector sort approach instead of a map

--- src/ws/region_graph.hpp
+++ src/ws/region_graph.hpp
@@ -5,50 +5,18 @@
 #include <cstddef>
 #include <iostream>
 
-// template <ID>
-// MapContainer<ID, ID> renumber(
-// 	volume<ID>& seg,
-// 	const ptrdiff_t sx, const ptrdiff_t sy, const ptrdiff_t sz
-
-// ) {
-// 	MapContainer<ID, ID> remap;
-
-// 	if (sx * sy * sz == 0) {
-// 		return remap;
-// 	}
-
-// 	remap.reserve(10000);
-
-// 	ID remap_id = 1;
-// 	ID last_elem = ~seg[0][0][0]; // isn't the first value and won't cause an overflow
-// 	ID last_remap_id = remap_id;
-
-// 	for (std::ptrdiff_t z = 0; z < sz; ++z) {
-// 		for (std::ptrdiff_t y = 1; y < sy; ++y) {
-// 			for (std::ptrdiff_t x = 1; x < sx; ++x) {
-// 				ID elem = seg[x][y][z];
-// 				if (elem == last_elem) {
-// 					seg[x][y][z] = last_remap_id;
-// 					continue;
-// 				}
-
-// 				if (remap.contains(elem)) {
-// 					seg[x][y][z] = remap[elem];
-// 				}
-// 				else {
-// 					seg[x][y][z] = remap_id;
-// 					remap[elem] = remap_id;
-// 					remap_id++;
-// 				}
-
-// 				last_elem = elem;
-//     		last_remap_id = seg[x][y][z];
-// 			}
-// 		}		
-// 	}
-
-// 	return remap;
-// }
+template <typename T>
+std::vector<T> apply_permutation(
+  const std::vector<T>& vec,
+  const std::vector<std::size_t>& p
+) {
+  std::vector<T> sorted_vec(vec.size());
+  std::transform(
+  	p.begin(), p.end(), sorted_vec.begin(),
+    [&](std::size_t i){ return vec[i]; }
+  );
+  return sorted_vec;
+}
 
 template< typename ID, typename F, typename L>
 inline region_graph<ID,F>
@@ -67,23 +35,23 @@ get_region_graph(
 	const std::ptrdiff_t sy = aff_ptr->shape()[1];
 	const std::ptrdiff_t sz = aff_ptr->shape()[2];
 
-	volume<ID>& seg = *seg_ptr;
-	// MapContainer<ID,ID> remap = renumber(seg, sx, sy, sz);
+	const std::ptrdiff_t voxels = sx * sy * sz;
 
+	volume<ID>& seg = *seg_ptr;
 	affinity_graph<F>& aff = *aff_ptr;
 	std::vector<id_pair> pairs;
 
-	std::vector<MapContainer<ID, F> > edges(max_segid);
-
-	// std::vector<emilib::HashMap<ID, F> > edges(remap.size() + 1);
-	for (auto& h : edges) {
-		h.reserve(10);
-	}
-
-	id_pair ids = std::pair<ID,ID>(max_segid + 1, max_segid + 1);
-	F* curr;
+	std::vector<uint64_t> edges;
+	std::vector<F> edge_values;
+	edges.reserve(voxels);
+	edge_values.reserve(voxels);
 
 	std::ptrdiff_t x, y, z;
+
+	// save edges as a uint64 like min(e1,e2)|max(e1,e2)
+	// this shift and mask are needed to encoding and decoding
+	const uint64_t shift = 8 * sizeof(ID);
+	const uint64_t mask = ~(reinterpret_cast<uint64_t>(-1) >> shift << shift);
 
 	auto maxfn = [&](std::ptrdiff_t ix, std::ptrdiff_t iy, std::ptrdiff_t iz, std::ptrdiff_t channel) {
 		if ( 
@@ -91,59 +59,60 @@ get_region_graph(
 			&& seg[ix][iy][iz] 
 			&& seg[x][y][z] != seg[ix][iy][iz]
 		) {
-			id_pair p = std::minmax(seg[x][y][z], seg[ix][iy][iz]);
-			if (p != ids) {
-				curr = &edges[p.first][p.second];
-				ids = p;	
-			}
-			if (!(*curr)) {
-				pairs.push_back(p);
-			}
-			*curr = std::max(*curr, aff[ix][iy][iz][channel]);
+			uint64_t edge = (seg[x][y][z] > seg[ix][iy][iz])
+				? (seg[x][y][z] | (seg[ix][iy][iz] << shift))
+				: (seg[ix][iy][iz] | (seg[x][y][z] << shift));
+
+			edges.push_back(edge);
+			edge_values.push_back(aff[ix][iy][iz][channel]);
 		}
 	};
 
-	const std::ptrdiff_t block_size = 128;
-
-	for (std::ptrdiff_t gz = 0; gz <= sz / block_size; gz++) {
-		for (std::ptrdiff_t gy = 0; gy <= sy / block_size; gy++) {
-			for (std::ptrdiff_t gx = 0; gx <= sx / block_size; gx++) {
-				std::ptrdiff_t z0 = (gz * block_size);
-				std::ptrdiff_t y0 = (gy * block_size);
-				std::ptrdiff_t x0 = (gx * block_size);
-
-				z = std::max(z0, static_cast<std::ptrdiff_t>(1));
-				y = std::max(y0, static_cast<std::ptrdiff_t>(1));
-				x = std::max(x0, static_cast<std::ptrdiff_t>(1));
-
-				for (int k = 0; k < std::min(static_cast<std::ptrdiff_t>(block_size), sz - z0 - 1); k++, z++) {
-					y = std::max(y0, static_cast<std::ptrdiff_t>(1));
-					for (int j = 0; j < std::min(static_cast<std::ptrdiff_t>(block_size), sy - y0 - 1); j++, y++) {
-						x = std::max(x0, static_cast<std::ptrdiff_t>(1));
-						for (int i = 0; i < std::min(static_cast<std::ptrdiff_t>(block_size), sx - x0 - 1); i++, x++) {
-
-							if (x > boundary_flags[0]) {
-								maxfn(x-1,y,z,0);
-							}
-							if (y > boundary_flags[1]) {
-								maxfn(x,y-1,z,1);
-							}
-							if (z > boundary_flags[2]) {
-								maxfn(x,y,z-1,2);
-							}
-						}	
-					}
+	for (z = 1; z < sz - 1; z++) {
+		for (y = 1; y < sy - 1; y++) {
+			for (x = 1; x < sx - 1; x++) {
+				if (x > boundary_flags[0]) {
+					maxfn(x-1,y,z,0);
+				}
+				if (y > boundary_flags[1]) {
+					maxfn(x,y-1,z,1);
+				}
+				if (z > boundary_flags[2]) {
+					maxfn(x,y,z-1,2);
 				}
 			}
 		}
 	}
 
 	region_graph<ID,F> rg;
-
-	for (const auto& p : pairs) {
-		auto v = edges[p.first][p.second];
-		rg.emplace_back(v, p.first, p.second);
+	if (edges.size() == 0) {
+		std::cout << "Region graph size: " << rg.size() << std::endl;
+		return rg;
 	}
+
+	std::vector<std::size_t> edge_permutation(edges.size()); // sort indices
+  std::iota(edge_permutation.begin(), edge_permutation.end(), 0);
+  std::sort(edge_permutation.begin(), edge_permutation.end(),
+        [&](std::size_t i, std::size_t j){ return edges[i] < edges[j] });
+
+  edges = apply_permutation(edges, edge_permutation);
+  edge_values = apply_permutation(edge_values, edge_permutation);
+
+  uint64_t edge = edges[0];
+  affinity_t max_affinity = edge_values[0];
+
+  for (std::ptrdiff_t i = 0; i < edges.size(); i++) {
+  	if (edges[i] == edge) {
+  		max_affinity = std::max(max_affinity, edge_values[i]);
+  	}
+  	else {
+  		ID e1 = edge & mask;
+  		ID e2 = edge >> shift;
+  		rg.emplace_back(max_affinity, e1, e2);
+  	}
+  }
+
+  rg.emplace_back(max_affinity, (edge & mask), (edge >> shift));
 
 	std::cout << "Region graph size: " << rg.size() << std::endl;
 
