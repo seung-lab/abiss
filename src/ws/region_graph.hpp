782409acc5afe691ce3fda1cac5dc1313a90b019
William Silversmith <william.silversmith@gmail.com>
2022-05-20 18:19:21 -0400

    perf: try old version with faster scan

--- src/ws/region_graph.hpp
+++ src/ws/region_graph.hpp
@@ -4,159 +4,92 @@
 
 #include <cstddef>
 #include <iostream>
-#include <limits>
-#include <cmath>
-#include <ctime>
-
-template <typename T>
-std::vector<T> apply_permutation(
-  const std::vector<T>& vec,
-  const std::vector<std::size_t>& p
-) {
-	const std::size_t N = vec.size();
-  std::vector<T> sorted_vec(N);
-  for (std::size_t i = 0; i < N; i++) {
-  	sorted_vec[p[i]] = vec[i];
-  }
-  return sorted_vec;
-}
 
 template< typename ID, typename F, typename L>
 inline region_graph<ID,F>
-get_region_graph(
-	const affinity_graph_ptr<F>& aff_ptr,
-	const volume_ptr<ID> seg_ptr,
-	std::size_t max_segid, 
-	const L& lowv, 
-	const std::array<bool,6> & boundary_flags
-) {
-	using affinity_t = F;
-	using id_pair = std::pair<ID,ID>;
-	affinity_t low  = static_cast<affinity_t>(lowv);
+get_region_graph( const affinity_graph_ptr<F>& aff_ptr,
+                  const volume_ptr<ID> seg_ptr,
+                  std::size_t max_segid, const L& lowv, const std::array<bool,6> & boundary_flags)
+{
+    using affinity_t = F;
+    using id_pair = std::pair<ID,ID>;
+    affinity_t low  = static_cast<affinity_t>(lowv);
 
-	const std::ptrdiff_t sx = aff_ptr->shape()[0];
-	const std::ptrdiff_t sy = aff_ptr->shape()[1];
-	const std::ptrdiff_t sz = aff_ptr->shape()[2];
+    const std::ptrdiff_t sx = aff_ptr->shape()[0];
+    const std::ptrdiff_t sy = aff_ptr->shape()[1];
+    const std::ptrdiff_t sz = aff_ptr->shape()[2];
+    const std::ptrdiff_t sxy = sx * sy;
+    const std::ptrdiff_t sxyz = sxy * sz;
 
-	const std::ptrdiff_t sxy = sx * sy;
-	const std::ptrdiff_t voxels = sx * sy * sz;
+		ID* seg = seg_ptr->data();
+		affinity_t* aff = aff_ptr->data();
 
-	// volume<ID>& seg_boost = *seg_ptr;
-	ID* seg = seg_ptr->data();
+    region_graph<ID,F> rg;
 
-	affinity_t* aff = aff_ptr->data();
-	std::vector<id_pair> pairs;
+    std::vector<id_pair> pairs;
 
-	std::vector<uint64_t> edges;
-	std::vector<F> edge_values;
-	edges.reserve(voxels);
-	edge_values.reserve(voxels);
-
-	// save edges as a uint64 like min(e1,e2)|max(e1,e2)
-	// this shift and mask are needed to encoding and decoding
-	const uint64_t shift = 8 * static_cast<uint64_t>(std::ceil(std::log2(max_segid)));
-	const uint64_t mask = ~(std::numeric_limits<uint64_t>::max() >> shift << shift);
-
-	clock_t begin = clock();
+    std::vector<MapContainer<ID, F> > edges(max_segid);
+    for (auto & h : edges) {
+        h.reserve(10);
+    }
 
 	for (std::ptrdiff_t z = 1; z < sz - 1; z++) {
 		for (std::ptrdiff_t y = 1; y < sy - 1; y++) {
 			for (std::ptrdiff_t x = 1; x < sx - 1; x++) {
 				std::ptrdiff_t loc = x + sx * y + sxy * z;
 
-				if ( 
+        if ( 
 					x > boundary_flags[0]
 					&& seg[loc]
 					&& seg[loc-1]
 					&& seg[loc] != seg[loc-1]
 				) {
-					uint64_t edge = (seg[loc] > seg[loc-1])
-						? (seg[loc] | (seg[loc-1] << shift))
-						: (seg[loc-1] | (seg[loc] << shift));
-
-					edges.push_back(edge);
-					edge_values.push_back(aff[(loc-1) + sxy * sz * 0]);
-				}
+            auto p = std::minmax(seg[loc], seg[loc-1]);
+            F& curr = edges[p.first][p.second];
+            if (!curr) {
+                pairs.push_back(p);
+            }
+            curr = std::max(curr, affaff[(loc-1) + sxyz * 0]);
+        }
 				if ( 
 					y > boundary_flags[1]
 					&& seg[loc] 
 					&& seg[loc-sx] 
 					&& seg[loc] != seg[loc-sx]
 				) {
-					uint64_t edge = (seg[loc] > seg[loc-sx])
-						? (seg[loc] | (seg[loc-sx] << shift))
-						: (seg[loc-sx] | (seg[loc] << shift));
-
-					edges.push_back(edge);
-					edge_values.push_back(aff[(loc-sx) + sxy * sz * 1]);
-				}
+            auto p = std::minmax(seg[loc], seg[loc-sx]);
+            F& curr = edges[p.first][p.second];
+            if (!curr) {
+                pairs.push_back(p);
+            }
+            curr = std::max(curr, affaff[(loc-sx) + sxyz * 1]);
+        }
 				if ( 
 					z > boundary_flags[2]
 					&& seg[loc] 
 					&& seg[loc-sxy] 
 					&& seg[loc] != seg[loc-sxy]
 				) {
-					uint64_t edge = (seg[loc] > seg[loc-sxy])
-						? (seg[loc] | (seg[loc-sxy] << shift))
-						: (seg[loc-sxy] | (seg[loc] << shift));
-
-					edges.push_back(edge);
-					edge_values.push_back(aff[(loc-sxy) + sxy * sz * 2]);
-				}
+            auto p = std::minmax(seg[loc], seg[loc-sxy]);
+            F& curr = edges[p.first][p.second];
+            if (!curr) {
+                pairs.push_back(p);
+            }
+            curr = std::max(curr, affaff[(loc-sxy) + sxyz * 2]);
+        }
 			}
 		}
 	}
-	clock_t end = clock();
-	std::cout << "Image scan (sec): " << double(end - begin) / CLOCKS_PER_SEC << std::endl;
 
-	region_graph<ID,F> rg;
-	if (edges.size() == 0) {
-		std::cout << "Region graph size: " << rg.size() << std::endl;
-		return rg;
-	}
+    for (const auto& p : pairs) {
+      auto v = edges[p.first][p.second];
+      rg.emplace_back(v, p.first, p.second);
+    }
 
-	begin = clock();
-	std::vector<std::size_t> edge_permutation(edges.size()); // sort indices
-  std::iota(edge_permutation.begin(), edge_permutation.end(), 0);
-  std::sort(edge_permutation.begin(), edge_permutation.end(),
-        [&](std::size_t i, std::size_t j){ return edges[i] < edges[j]; });
-  end = clock();
-	std::cout << "Create permutation (sec): " << double(end - begin) / CLOCKS_PER_SEC << std::endl;
+    std::cout << "Region graph size: " << rg.size() << std::endl;
 
-	begin = clock();
-  edges = std::move(apply_permutation(edges, edge_permutation));
-  edge_values = std::move(apply_permutation(edge_values, edge_permutation));
-  end = clock();
-  std::cout << "Sort vectors (sec): " << double(end - begin) / CLOCKS_PER_SEC << std::endl;
+    std::stable_sort(std::begin(rg), std::end(rg), [](auto & a, auto & b) { return std::get<0>(a) > std::get<0>(b); });
 
-
-  begin = clock();
-  uint64_t edge = edges[0];
-  affinity_t max_affinity = edge_values[0];
-
-  for (std::ptrdiff_t i = 0; i < edges.size(); i++) {
-  	if (edges[i] == edge) {
-  		max_affinity = std::max(max_affinity, edge_values[i]);
-  	}
-  	else {
-  		ID e1 = edge & mask;
-  		ID e2 = edge >> shift;
-  		rg.emplace_back(max_affinity, e1, e2);
-  	}
-  }
-
-  rg.emplace_back(max_affinity, (edge & mask), (edge >> shift));
-
-	end = clock();
-  std::cout << "Create region graph (sec): " << double(end - begin) / CLOCKS_PER_SEC << std::endl;
-
-	std::cout << "Region graph size: " << rg.size() << std::endl;
-
-	// begin = clock();
-	// std::stable_sort(std::begin(rg), std::end(rg), [](auto & a, auto & b) { return std::get<0>(a) > std::get<0>(b); });
-	// end = clock();
-	// std::cout << "Sort region graph (sec): " << double(end - begin) / CLOCKS_PER_SEC << std::endl;
-
-	std::cout << "Sorted" << std::endl;
-	return rg;
+    std::cout << "Sorted" << std::endl;
+    return rg;
 }
