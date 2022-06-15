d681eb3585a46f9fb9eec2a2084a9b6d92f23b4b
Ran Lu <ranl@princeton.edu>
2022-06-15 13:40:13 -0400

    Do not upload the empty remap files

--- scripts/atomic_chunk_ws.sh
+++ scripts/atomic_chunk_ws.sh
@@ -7,12 +7,9 @@ try acquire_cpu_slot
 try mkdir remap
 try taskset -c $cpuid python3 $SCRIPT_PATH/cut_chunk_ws.py $1
 try taskset -c $cpuid $BIN_PATH/ws param.txt aff.raw $WS_HIGH_THRESHOLD $WS_LOW_THRESHOLD $WS_SIZE_THRESHOLD $output
-try touch remap_"${output}".data
 try touch ongoing_"${output}".data
 try $COMPRESS_CMD seg_"${output}".data
 retry 10 $UPLOAD_ST_CMD seg_"${output}".data."${COMPRESSED_EXT}" $FILE_PATH/seg/seg_"${output}".data."${COMPRESSED_EXT}"
-try $COMPRESS_CMD remap_"${output}".data
-retry 10 $UPLOAD_ST_CMD remap_"${output}".data."${COMPRESSED_EXT}" $FILE_PATH/remap/remap_"${output}".data."${COMPRESSED_EXT}"
 try md5sum *_"${output}".data > "${output}".data.md5sum
 try $COMPRESS_CMD ongoing_"${output}".data
 retry 10 $UPLOAD_ST_CMD ongoing_"${output}".data."${COMPRESSED_EXT}" $FILE_PATH/remap/ongoing_"${output}".data."${COMPRESSED_EXT}"
