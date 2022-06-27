df75d785cee5e748bca2b2b7d253bcaee403319e
Ran Lu <ranl@princeton.edu>
2022-06-27 00:06:36 -0400

    Decode redis value before comparison
    The value returned by redis is a bytearray, comparing it with a string
    is always false

--- scripts/check_task_flag.py
+++ scripts/check_task_flag.py
@@ -8,7 +8,7 @@ TASK_KEY = sys.argv[1]
 
 if "REDIS_SERVER" in os.environ:
     r = redis.Redis(os.environ["REDIS_SERVER"])
-    if r.exists(TASK_KEY) and r.get(TASK_KEY) == "DONE":
+    if r.exists(TASK_KEY) and r.get(TASK_KEY).decode() == "DONE":
         sys.exit(0)
     else:
         sys.exit(1)
