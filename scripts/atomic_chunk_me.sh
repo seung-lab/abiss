fc1578a4967a97b8721ac7785cecfa5839514a22
Ran Lu <ranl@princeton.edu>
2022-06-26 23:05:18 -0400

    Download chunkmap files with cloudfiles
    This is more efficient comparing with the old approach where we generate
    the file list and then feed it to gsutil.

--- scripts/atomic_chunk_me.sh
+++ scripts/atomic_chunk_me.sh
@@ -15,10 +15,6 @@ for d in $META; do
     try mkdir $d
 done
 
-try python3 $SCRIPT_PATH/generate_siblings.py $1|tee filelist.txt
-try cat filelist.txt|$PARALLEL_CMD "$DOWNLOAD_ST_CMD $CHUNKMAP_INPUT/chunkmap_{}.data.${COMPRESSED_EXT} - | $COMPRESS_CMD -d -o chunkmap_{}.data"
-try cp chunkmap_${output}.data localmap.data
-
 try python3 $SCRIPT_PATH/merge_chunkmap.py $1
 
 try taskset -c $cpuid python3 $SCRIPT_PATH/cut_chunk_agg.py $1
