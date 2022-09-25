ee233bc5f03401c29bc8cdfb2348ffa7ec613c44
Ran Lu <ranl@princeton.edu>
2022-09-25 00:18:28 -0400

    Heartbeat to redis during health check
    Upload a timestamp to the first database of redis, with the hostname as
    the key, the bot can use the timestamp to decide if the worker is still
    healthy

--- scripts/health_check.py
+++ scripts/health_check.py
@@ -1,5 +1,25 @@
-import psutil
 import sys
+import json
+import requests
+import psutil
+import redis
+from datetime import datetime
+
+METADATA_URL = "http://metadata.google.internal/computeMetadata/v1/instance/"
+METADATA_HEADERS = {'Metadata-Flavor': 'Google'}
+
+def get_hostname():
+    data = requests.get(METADATA_URL + 'hostname', headers=METADATA_HEADERS).text
+    return data.split(".")[0]
+
+
+with open("/run/secrets/param") as f:
+    data = json.load(f)
+    if "REDIS_SERVER" in data:
+        hostname = get_hostname()
+        r = redis.Redis(host=data["REDIS_SERVER"])
+        r.set(hostname, datetime.now().timestamp())
+
 
 cpu_percent = sum(psutil.cpu_percent(5, percpu=True))
 
