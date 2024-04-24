166325c47af13638fd1498c40ced9734399a5b79
trivoldus28 <tri@zetta.ai>
2024-04-24 21:06:50 +0000

    Disable unneeded libs

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 from setuptools import setup, find_packages
 from setuptools.extension import Extension
-# from setuptools.command.build_ext import build_ext as _build_ext
-# from Cython.Build import cythonize
 import os
-# import builtins
 import numpy
 
 # VERSION = '1.0'
@@ -11,7 +8,6 @@ import numpy
 source_dir = os.path.dirname(os.path.abspath(__file__))
 include_dirs = [
     os.path.join(source_dir, 'src'),
-    # os.path.dirname(get_python_inc()),
     numpy.get_include(),
 ]
 
@@ -24,7 +20,7 @@ extensions = [
         ],
         include_dirs=include_dirs,
         language='c++',
-        extra_link_args=['-lboost_iostreams', '-fopenmp', '-ltbb'],
+        # extra_link_args=['-lboost_iostreams', '-fopenmp', '-ltbb'],  # only necessary for when agg is included
         extra_compile_args=['-std=c++20', '-w'],
         # undef_macros=["NDEBUG"],
     )
