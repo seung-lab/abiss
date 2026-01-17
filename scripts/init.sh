befa4e47ae5acf7c7c6977de512f02135d75866e
Ran Lu <ranl@princeton.edu>
2026-01-16 20:04:33 -0500

    Better retry function
    Do not flatten the parameters into a string

--- scripts/init.sh
+++ scripts/init.sh
@@ -7,9 +7,8 @@ try_to_skip() { "$@" && bail "skip $*";  }
 just_in_case() { "$@" || true; }
 retry() {
     local -r -i max_attempts="$1"; shift
-    local -r cmd="$@"
     local -i attempt_num=1
-    until $cmd
+    until "$@"
     do
         if ((attempt_num==max_attempts))
         then
